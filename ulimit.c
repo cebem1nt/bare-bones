0f17c138b3047b88d0a3d5b2e289380bcb801c7f
cebem1nt <mineewarik@gmail.com>
2025-10-08 21:57:51 -0300

    Raw ulimit version

---
+++ ulimit.c
@@ -0,0 +1,38 @@
+/*
+ * Simple ulimit-like program to print 
+ * different limits for a machine
+ */
+
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void err_exit()
+{
+    perror("unlimit"); 
+    exit(EXIT_FAILURE);
+}
+
+int
+main()
+{
+    long int open_file;
+    errno = 0;
+
+#ifdef OPEN_MAX
+    open_file = sysconf(OPEN_MAX);
+#else
+    // Lets assume that _SC_OPEN_MAX is set otherwise
+    open_file = sysconf(_SC_OPEN_MAX);
+#endif
+
+    if (open_file > 0)
+        printf("Open files: \t %li\n", open_file);
+    else if (open_file == -1 && errno == 0) 
+        printf("Open files: \t unlimited\n");
+    else
+        err_exit();
+
+    return 0;
+}
