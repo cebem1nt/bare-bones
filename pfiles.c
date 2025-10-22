211b8d46c33e453eeb1e9d4ce49d237a21aad2dd
cebem1nt <mineewarik@gmail.com>
2025-10-22 01:32:23 -0300

    Little bit improved pfiles, added more info

--- pfiles.c
+++ pfiles.c
@@ -5,14 +5,18 @@
  * So what we just have to readlink of each file based on given pid
  */
 
+#include <sys/stat.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pwd.h>
+#include <time.h>
 
 #define MAX_FD_LENGTH 256
 #define MAX_FILE_PATH 1024
+#define MAX_TIME_LEN 20
 
 size_t
 calc_size(char* pid) 
@@ -27,11 +31,41 @@ err_exit(const char* message)
     exit(EXIT_FAILURE);
 }
 
+void
+read_fd_link(char* link, char* dest) 
+{
+    size_t len;
+    len = readlink(link, dest, MAX_FILE_PATH - 1);
+    if (len == -1)
+        err_exit("readlink");
+    
+    dest[len] = '\0';
+}
+
+char*
+uid_to_name(uid_t uid) 
+{
+    struct passwd* p = getpwuid(uid);
+    return p->pw_name;
+}
+
+void format_time(time_t t, char* buf, size_t len) 
+{
+    struct tm tm;
+
+    if (localtime_r(&t, &tm) == NULL) {
+        buf[0] = '\0';
+        return;
+    }
+
+    strftime(buf, len, "%m-%d %H:%M:%S", &tm);
+}
+
 int
 main(int argc, char** argv)
 {
     if (argc != 2) {
-        printf("Usage: pfiles <pid>");
+        printf("Usage: pfiles <pid>\n");
         return 1;
     }
 
@@ -55,25 +89,30 @@ main(int argc, char** argv)
         err_exit("opendir");
 
     while ((dirp = readdir(dp)) != NULL) {
-        char* fd = dirp->d_name;
+        char   file_path[MAX_FILE_PATH];
+        char   time_buf[MAX_TIME_LEN];
 
-        if (strcmp(fd, ".") == 0 || strcmp(fd, "..") == 0)
+        char*  proc_fd = dirp->d_name;
+        struct stat st;
+
+        if (strcmp(proc_fd, ".") == 0 || strcmp(proc_fd, "..") == 0)
             continue;
 
-        char*  link = malloc(proc_path_size + MAX_FD_LENGTH);
-        char   file_path[MAX_FILE_PATH];
-        size_t file_path_len;
+        char* link = malloc(proc_path_size + MAX_FD_LENGTH);
+        if (!link)
+            err_exit("malloc");
 
         strcpy(link, proc_path);
-        strcat(link, fd);
+        strcat(link, proc_fd);
 
-        file_path_len = readlink(link, file_path, MAX_FILE_PATH - 1);
-
-        if (file_path_len == -1)
-            err_exit("readlink");
+        read_fd_link(link, file_path);
+        if (stat(link, &st) == -1)
+            err_exit("stat");
         
-        file_path[file_path_len] = '\0';
-        printf("%s \t %s\n", fd, file_path);
+        char* owner = uid_to_name(st.st_uid);
+        format_time(st.st_mtim.tv_sec, time_buf, MAX_TIME_LEN);
+
+        printf("%s \t %s %s - %s\n", proc_fd, time_buf, owner, file_path);
         free(link);
     }
 
