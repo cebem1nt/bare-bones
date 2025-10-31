414b9f3f95bff2fb9ea2935eba1893aa1e74ab95
cebem1nt <mineewarik@gmail.com>
2025-10-30 22:29:12 -0300

    Made file encryption for xore

--- xore.c
+++ xore.c
@@ -1,15 +1,15 @@
 /*
  * Simple XOR encryption
- * TODO Encrypt filess
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <termios.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 #define MAX_KEY_SIZE 256
+#define CHUNK_SIZE 4096
 
 void
 err_exit(char* reason)
@@ -40,21 +40,35 @@ get_key(char* dest)
 int 
 main(int argc, char** argv) 
 {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s: <text>\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s: <input file> <output file>\n", argv[0]);
         return 1;
     }
 
     char   key[MAX_KEY_SIZE];
-    char*  data = argv[1];
-    size_t len = strlen(data);
+    char   buf[CHUNK_SIZE];
+
+    int in_fd, out_fd;
+    int n;
 
     get_key(key);
 
-    for (int i = 0; i < len; i++) {
-        data[i] ^= key[i % len];
+    if ((in_fd = open(argv[1], O_RDONLY)) == -1)
+        err_exit("open input");
+
+    if ((out_fd = open(argv[2], O_WRONLY | O_CREAT, 0644)) == -1)
+        err_exit("open output");
+
+    while ((n = read(in_fd, buf, CHUNK_SIZE)) != 0) {
+        if (n == -1) 
+            err_exit("read");
+            
+        for (int i = 0; i < n; i++) {
+            buf[i] ^= key[i % n];
+        }
+
+        write(out_fd, buf, n);
     }
 
-    printf("%s\n", data);
     return 0;
 }
